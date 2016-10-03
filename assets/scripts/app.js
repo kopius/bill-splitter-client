7a7737c50557190d875ee109dc755eb47f459aa7
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-03 14:46:27 -0400

    Try making change to app.host before branching
    Try making change to app.host before branching

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,9 @@
+
 'use strict';
 
 const app = {
   host: 'https://bill-splitter-api.herokuapp.com/'
-  // replace with heroku address before deploying:
-  // https://bill-splitter-api.herokuapp.com/
+  // replace with heroku address before deploying
 };
 
 module.exports = app;
