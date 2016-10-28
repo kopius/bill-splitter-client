03ae0936230644698026c845fa30c0b2127b344c
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-28 15:50:01 -0400

    Clean up app.js
    - remove comment about switching between local and deployed host

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,6 @@
 
 const app = {
   host: 'https://bill-splitter-api.herokuapp.com'
-  // replace with heroku address before deploying
-  // https://bill-splitter-api.herokuapp.com
 };
 
 module.exports = app;
